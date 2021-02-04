3977c0f8555a7d7fa0581f13ab6b4a5c19573b60
Marek Landa <landama2@fel.cvut.cz>
2021-02-04 08:26:26 +0100

    Add missing script for Chrome install

---
+++ src/test/java/cz/cvut/fel/skillmatrix/frontend/screenplay/pageobjects/RegistrationPage.java
@@ -0,0 +1,36 @@
+package cz.cvut.fel.skillmatrix.frontend.screenplay.pageobjects;
+
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class RegistrationPage {
+
+    public static final Target usernameField = Target
+        .the("Username field")
+        .located(By.id("username"));
+
+    public static final Target firstNameField = Target
+        .the("First name field")
+        .located(By.id("name"));
+
+    public static final Target surnameField = Target
+        .the("Surname field")
+        .located(By.id("surname"));
+
+    public static final Target emailField = Target
+        .the("Email field")
+        .located(By.id("email"));
+
+    public static final Target passwordField = Target
+        .the("Password field")
+        .located(By.id("password"));
+
+    public static final Target confirmPasswordField = Target
+        .the("Confirm password field")
+        .located(By.id("confirmPassword"));
+
+    public static final Target submitButton = Target
+        .the("submitButton")
+        .located(By.xpath("/html/body/jhi-main/div[2]/div/jhi-register/div/div[2]/div/form/button"));
+
+}
